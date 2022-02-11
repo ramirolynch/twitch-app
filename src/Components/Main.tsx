0d18e1d6fe9e8d87ccf4505c1fa1f700f33f6adb
Dylan <dlokatys@hotmail.com>
2022-02-10 19:24:54 -0500

    changed ResultsList to Search Results. added Result componenet

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { ResultsList } from "./ResultsList";
+import { SearchResults } from "./SearchResults";
 
 
 export function Main() {
--- src/Components/SearchResults.tsx
+++ src/Components/SearchResults.tsx
@@ -1,6 +1,6 @@
 
 
-export function ResultsList() {
+export function SearchResults() {
 
     return (
 
