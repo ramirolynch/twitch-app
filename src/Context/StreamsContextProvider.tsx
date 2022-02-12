6d02b94a9cbde8cf79205ffee5d1abd490f41e1f
ramirolynch <ramirolynch@yahoo.com>
2022-02-12 10:15:13 -0600

    added useEffect to get the stream in  src/Context/StreamsContextProvider.tsx

--- src/Context/StreamsContextProvider.tsx
+++ src/Context/StreamsContextProvider.tsx
@@ -1,14 +1,25 @@
-import {  ReactNode, useState } from "react";
+import {  ReactNode, useEffect, useState } from "react";
 import { isTemplateSpan } from "typescript";
-import { Stream } from "../Models/Stream";
+import { Stream, Streams } from "../Models/Stream";
 import { StreamContext } from "../Context/StreamsContext";
+import { getStreamResponse, getTop } from "../Service/TwitchApi";
+
 
 
 interface Props {children:ReactNode;}
 
 export function StreamContextProvider({children}:Props) {
 
-    const [streamList, setStreamList] = useState<Stream[]>([]) // between parenthesis we put the list of streams that we want in the context.
+    const[streams, setStreams] = useState<Stream[]>([]);
+
+       useEffect(() =>{
+    
+        getStreamResponse().then(response => setStreams(response.data));
+    
+
+       }, []);
+
+    const [streamList, setStreamList] = useState<Stream[]>(streams) // between parenthesis we put the list of streams that we want in the context.
     const [favorites, setFavorites] = useState<Stream[]>([])
 
     function addFave(stream:Stream) {
