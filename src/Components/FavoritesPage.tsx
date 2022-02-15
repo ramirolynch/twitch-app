33b6c6a5987597cdbc27cffbca250354a9b8f5dd
ramirolynch <ramirolynch@yahoo.com>
2022-02-14 23:09:29 -0600

    implemented favorites for trending streams

--- src/Components/FavoritesPage.tsx
+++ src/Components/FavoritesPage.tsx
@@ -1,11 +1,18 @@
+import { useContext } from "react";
+import { StreamContext } from "../Context/StreamsContext";
+import { TrendingResult } from "./TrendingResult";
 
 
 export function FavoritesPage() {
 
-    return (
+    const {favorites} = useContext(StreamContext)
+
+   
+
+    return(
 
         <div>
-            
+            {favorites.map((stream, i) => <TrendingResult key={i} stream={stream}></TrendingResult>)}
         </div>
     );
 }
