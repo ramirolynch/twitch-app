21ccbdf7b598e806bb1af25684535613d7cdcacb
brittany <brioden88@gmail.com>
2022-02-16 01:28:56 -0500

    add external twitch link

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export function Header() {
 
+    
     return (
 
         <div className='header'>
-            <h1>Twitch </h1>
+            <h1><a target="_blank" href="https://www.twitch.tv/">Twitch</a></h1> 
+            
             <NavLink to='/'>Trending</NavLink>
             <NavLink to='/TopGames'>Top Games</NavLink>
             <NavLink to='/Search'>Search</NavLink>
